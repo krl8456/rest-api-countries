3ba4e486e7b236ce5be19b2dbf6fb1ef6dca97e5
krl8456 <karolrak50@gmail.com>
2022-08-09 01:20:25 +0200

    Add initial files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        DarkBlue: "hsl(209, 23%, 22%)",
+        VeryDarkBlueBackground: "hsl(207, 26%, 17%)",
+        VeryDarkBlueText: "hsl(200, 15%, 8%)",
+        DarkGray: "hsl(0, 0%, 52%)",
+        VeryLightGray: "hsl(0, 0%, 98%)",
+        White: "hsl(0, 0%, 100%)"
+      },
+      fontFamily: {
+        nunito: ['NunitoSans']
+      }
+    },
+  },
+  plugins: [],
+}
